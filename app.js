6676096062526a0798ec0a0aeeecb9cc53ec1f0f
SKM-MAC <social@saadkhanmalik.com>
2016-09-08 00:12:39 +0300

    Basic Working Model
    - checks team tree house for user profile —> prints it
    -displays custom messages for basic errors

--- app.js
+++ app.js
@@ -3,62 +3,71 @@
 
 var https = require("https");
 var username ="saadkhanmalik";
+//var username ="saadkhanmalik123"; // Use to check statusCode error
 
-
+//print message function
 function printMessage(username, badgeCount, points) {
   var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript";
   console.log(message);
 }
 
-
-//connect to the api url:
-//https://teamtreehouse.com/saadkhanmalik.json
-var request = https.get("https://teamtreehouse.com/" + username + ".json", function(response) {
-  //console.log(response.statusCode); //Used the to innitially check connection.
-  var body = "";
-  //read the data
-  response.on('data', function (chunk){
-    body += chunk;
-  });
-  response.on('end', function(){
-    // console.log(body);
-    // console.log(typeOf body);
-    var profile = JSON.parse(body);
-    var totalPoints = ( profile.points.HTML +
-                        profile.points.CSS +
-                        profile.points.Design +
-                        profile.points.JavaScript +
-                        profile.points.Ruby +
-                        profile.points.PHP +
-                        profile.points.WordPress +
-                        profile.points.iOS +
-                        //profile.points.'Development Tools' +
-                        profile.points.Business +
-                        profile.points.Python +
-                        profile.points.Java +
-                        // profile.points.'Digital Literacy' +
-                        // profile.points.'Game Development' +
-                        // profile.points.'C#' +
-                        profile.points.Databases
-                      )
-
-    console.log(totalPoints);
-    printMessage(username, profile.badges.length, profile.points.JavaScript)
-    //console.dir(profile); //prints entier profile
-
-
-
-
-
-
-  });
-  //parse the data
-  //print the data out
-});
-
-
-
-// error print out
-request.on("error", function(error){
+//print out  error message function
+function printError(error){
   console.error(error.message);
-});
+
+}
+
+
+  //connect to the api url:
+  //https://teamtreehouse.com/saadkhanmalik.json
+  var request = https.get("https://teamtreehouse.com/" + username + ".json", function(response) {
+    //console.log(response.statusCode); //Used the to innitially check connection.
+    var body = "";
+    //read the data
+    response.on('data', function (chunk){
+      body += chunk;
+    });
+    response.on('end', function(){
+      if(response.statusCode === 200){
+
+        try {
+          // console.log(body);
+          // console.log(typeOf body);
+          //parse the data
+          var profile = JSON.parse(body);
+          var totalPoints = ( profile.points.HTML +
+                              profile.points.CSS +
+                              profile.points.Design +
+                              profile.points.JavaScript +
+                              profile.points.Ruby +
+                              profile.points.PHP +
+                              profile.points.WordPress +
+                              profile.points.iOS +
+                              profile.points['Development Tools'] +
+                              profile.points.Business +
+                              profile.points.Python +
+                              profile.points.Java +
+                              profile.points['Digital Literacy'] +
+                              profile.points['Game Development'] +
+                              profile.points['C#'] +
+                              profile.points.Databases
+          );
+          //print the data out
+          console.log(totalPoints);
+          printMessage(username, profile.badges.length, profile.points.JavaScript);
+          //console.dir(profile); //prints entier profile
+        } catch (error) {
+          //prse error
+          printError(error);
+
+        };
+      } else {
+        printError({message: "There was an error getting the profile for " + username + ". (" + response.statusCode + ")" });
+      };
+    });
+
+
+  });
+
+  // connection error
+  request.on("error", printError);
